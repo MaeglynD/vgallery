563a05cd97643b306679fd0a82ca42ac04483420
MaeglynD <31933322+MaeglynD@users.noreply.github.com>
2019-12-14 16:02:42 +0000

    added more items to collections

--- components/carousel.vue
+++ components/carousel.vue
@@ -5,17 +5,6 @@
           <span class="g-panel-descriptor">{{x.name}}</span>
       </slide>
     </carousel-3d>
-  
-    <!-- <div class="g-collection-simple-container">
-        <simplebar class="height-fix g-collection-core">
-        <div class="g-collection-panel" v-for="x in collectionPanels" :key="x.name">
-        <!-- <img src="../assets/offline-placeholder.png" alt="">
-        
-        <img :src="x.bg">
-        <span class="g-panel-descriptor">{{x.name}}</span>
-    </div>
-    </simplebar>
-    </div> -->
 </template>
 
 <script>
@@ -32,10 +21,9 @@ export default {
         {name: 'pencil', bg: 'https://cdn.discordapp.com/attachments/519416362607837198/655099475458654250/pencil-min.png', icon: 'mdi-pencil'},
         {name: 'pastel', bg: 'https://cdn.discordapp.com/attachments/519416362607837198/655099471255961627/pastel-min.png', icon: 'mdi-grease-pencil'},
         {name: 'paint', bg: 'https://cdn.discordapp.com/attachments/519416362607837198/655099476297515019/paint-min.png', icon: 'mdi-brush'},
-        {name: 'paint1', bg: 'https://cdn.discordapp.com/attachments/519416362607837198/655099476297515019/paint-min.png', icon: 'mdi-brush'},
-        {name: 'paint2', bg: 'https://cdn.discordapp.com/attachments/519416362607837198/655099476297515019/paint-min.png', icon: 'mdi-brush'},
-        {name: 'paint3', bg: 'https://cdn.discordapp.com/attachments/519416362607837198/655099476297515019/paint-min.png', icon: 'mdi-brush'},
-        {name: 'paint4', bg: 'https://cdn.discordapp.com/attachments/519416362607837198/655099476297515019/paint-min.png', icon: 'mdi-brush'},
+        {name: 'pen', bg: 'https://cdn.discordapp.com/attachments/519416362607837198/655435257931235360/pen-min.png', icon: 'mdi-brush'},
+        {name: 'college', bg: 'https://cdn.discordapp.com/attachments/519416362607837198/655437345348124713/college-min.png', icon: 'mdi-brush'},
+        // {name: 'sketches', bg: 'https://cdn.discordapp.com/attachments/519416362607837198/655099476297515019/paint-min.png', icon: 'mdi-brush'},
 
       ]
     }
